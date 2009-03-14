7c21eaa66f4ec30e26b937f795adfe99c2994282
Jonathan Rockway <jon@jrock.us>
2009-03-14 11:49:21 -0500

    speed up histogram report (oops!)

--- lib/BigBand/Report/Histogram.pm
+++ lib/BigBand/Report/Histogram.pm
@@ -19,17 +19,21 @@ sub add_sample {
 
     my $song = $sample->song_id;
     my $time = $sample->start;
+    my $dur = $sample->duration;
 
-    for( my $i = 0; $i < $sample->duration; $i++){
-        $self->song_data->{$song}[int(($time + $i)/3000)]++;
+    $self->song_data->{$song} ||= [];
+    my $song_data = $self->song_data;
+
+    my $i;
+    for( $i = 0; $i < $dur; $i+=10){
+        $song_data->{$song}[int(($time + $i)/1000)]+=10;
     }
-
     return;
 }
 
 sub histogram_for {
     my ($self, $song) = @_;
-    return [ map { $_ / 3000 } @{$self->song_data->{$song}} ];
+    return [ map { ( $_ || 0 ) / 1000 } @{$self->song_data->{$song}} ];
 }
 
 1;
